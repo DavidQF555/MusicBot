f562a8f86a6a973f3a2e50fc9847ea77ab84ad39
DavidQF555 <davidfeng2003@gmail.com>
2021-10-03 00:08:46 -0700

    Improved Queue Command

--- src/commands/queue.js
+++ src/commands/queue.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const { schedulers } = require('../audio.js');
 const { createSimpleFailure } = require('../util.js');
+const { AudioPlayerStatus } = require('@discordjs/voice');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,17 +10,17 @@ module.exports = {
 		.setDescription('Displays the queued tracks'),
 	async execute(interaction) {
 		const scheduler = schedulers.get(interaction.guildId);
-		if(!scheduler) {
+		if(!scheduler || scheduler.queue.length == 0) {
 			await interaction.reply(createSimpleFailure('Nothing is currently queued'));
 			return;
 		}
-		const message = new MessageEmbed().setTitle('Queue');
+		const message = new MessageEmbed().setTitle('Queue').setColor('#0x00FF00');
 		let desc = '';
 		for (let i = 0; i < scheduler.queue.length; i++) {
 			const track = scheduler.queue[i];
 			let line = i + 1 + '. [' + track.title.substring(0, 40) + '](' + track.url + ')';
-			if(i === scheduler.index) {
-				line = '**' + line + '**';
+			if(i == scheduler.index && scheduler.player.state.status !== AudioPlayerStatus.Idle) {
+				line = '> **' + line + '**';
 			}
 			desc += line + '\n';
 		}
