e7e3c2b7934b6d2155e7ff517f7156225bf2b937
DavidQF555 <54233412+DavidQF555@users.noreply.github.com>
2023-03-20 17:53:21 -0500

    Updated Packages

--- src/util.js
+++ src/util.js
@@ -2,12 +2,12 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports.createSimpleFailure = message => {
 	return {
-		embeds: [new EmbedBuilder().setDescription(message).setColor('#0xFF0000')],
+		embeds: [new EmbedBuilder().setDescription(message).setColor(0xFF0000)],
 	};
 };
 
 module.exports.createSimpleSuccess = message => {
 	return {
-		embeds: [new EmbedBuilder().setDescription(message).setColor('#0x00FF00')],
+		embeds: [new EmbedBuilder().setDescription(message).setColor(0x00FF00)],
 	};
 };
